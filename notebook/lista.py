202cbf2b25ee4b9d0bde1dce3c60066bd39ea0f3
samuelcarlos2469 <samuelcarlos701estudos@gmail.com>
2023-08-27 23:38:55 -0400

    terminando tarefa Pedro

--- notebook/lista.py
+++ notebook/lista.py
@@ -18,10 +18,34 @@ class Lista:
                 if desc != None: t.desc = desc
                 if prio != None: t.prio = prio
                 if conclusion != None: t.conclusion = conclusion
+                break
 
-    def get_tarefa(self,filtro=None,unfinished=None):
-        lista = sorted (self.lista,reverse=True)
-        if unfinished == True:
-            lista = [t for t in lista
-                     if t.conclusion != True]
+    def get_tarefa(self,filtro=None,unfinished=False):
+        self.lista = sorted (self.lista,reverse=True)
+        lista = list()
+
+        lista = [t for t in self.lista if not t.conclusion]#primeiro as nao concluidas
+
+        if unfinished != True:
+            lista += [t for t in self.lista
+                     if t.conclusion == True]#add as concluidas tbm
+            
+        if filtro:
+            for t in self.lista: 
+                if filtro in t.title or filtro in t.desc:
+                    lista.append(t)
+        return lista
+        
+    def find_tarefa(self, id): #ex1 slide 2
+        for t in self.lista:
+            if t.id == id:
+                return t
+
+    
+
+    
+
+
+
+    
 
